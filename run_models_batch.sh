5648065c615047c9880776a6ab28b79c256b8df5
Zakaria Coulibaly <47373846+levisstrauss@users.noreply.github.com>
2026-01-13 10:43:27 -0500

    Update run_models_batch.sh

--- run_models_batch.sh
+++ run_models_batch.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 # */AIPND-revision/intropyproject-classify-pet-images/run_models_batch.sh
-#                                                                             
-# PROGRAMMER: Jennifer S.
-# DATE CREATED: 02/08/2018                                  
-# REVISED DATE: 02/27/2018  - 
+
 # PURPOSE: Runs all three models to test which provides 'best' solution.
 #          Please note output from each run has been piped into a text file.
 #
